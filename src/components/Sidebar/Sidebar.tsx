392c81afa0af386af6488b7bb1851c50d17c9411
Maciej Klis <klismaciej.97@gmail.com>
2022-11-17 19:35:48 +0100

    Implemented basic router in side bar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,3 +1,4 @@
+import { Link, useLocation } from "react-router-dom";
 import classes from "./Sidebar.module.css"
 import MenuList from '@mui/material/MenuList';
 import MenuItem from "@mui/material/MenuItem";
@@ -8,35 +9,41 @@ import IosShareIcon from '@mui/icons-material/IosShare';
 import FeedSharpIcon from '@mui/icons-material/FeedSharp';
 
 const Sidebar = () => {
+    const location = useLocation();
+
     return (
         <div className={`${classes.sidebar}`}>
             <MenuList sx={{
                 width: '100%',
             }}>
-                <MenuItem>
-                    <ListItemIcon>
-                        <DashboardCustomizeIcon
-                            fontSize="medium" 
-                            sx={{
-                                color: '#fff',
-                                height: '45px',
-                            }}
-                        />
-                    </ListItemIcon>
-                    <ListItemText>Dashboards</ListItemText>
-                </MenuItem>
-                <MenuItem selected={true}>
-                    <ListItemIcon>
-                        <IosShareIcon 
-                            fontSize="medium" 
-                            sx={{
-                                color: '#fff',
-                                height: '45px',
-                            }}
-                        />
-                    </ListItemIcon>
-                    <ListItemText>Send Transaction</ListItemText>
-                </MenuItem>
+                <Link to="/React-app/">
+                    <MenuItem selected={'/React-app/' === location.pathname}>
+                        <ListItemIcon> 
+                            <DashboardCustomizeIcon
+                                fontSize="medium" 
+                                sx={{
+                                    color: '#fff',
+                                    height: '45px',
+                                }}
+                            />
+                        </ListItemIcon>
+                        <ListItemText>Dashboards</ListItemText>
+                    </MenuItem>
+                </Link>
+                <Link to="/React-app/send-transaction">
+                    <MenuItem selected={'/React-app/send-transaction' === location.pathname}>
+                        <ListItemIcon>
+                            <IosShareIcon 
+                                fontSize="medium" 
+                                sx={{
+                                    color: '#fff',
+                                    height: '45px',
+                                }}
+                            />
+                        </ListItemIcon>
+                        <ListItemText>Send Transaction</ListItemText>
+                    </MenuItem>
+                </Link>
                 <MenuItem disabled={true}>
                     <ListItemIcon>
                         <FeedSharpIcon 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  BrowserRouter as Router, Routes, Route, useLocation
+} from 'react-router-dom';
 import App from './App'
 import './index.css'
+import Dashboard from './pages/Dashboard';
+import SendTransaction from './pages/SendTransaction/index'
+
+const base = "/React-app"
+
+// const router = createBrowserRouter([
+//   {
+//     path: base + "/",
+//     element: SendTransaction,
+//   },
+// ]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+      <Routes>
+        <Route path={base + "/"} element={<Dashboard />}/>
+        <Route path={base + "/send-transaction"} element={<SendTransaction />}/>
+      </Routes>
+    </Router>
   </React.StrictMode>
 )
